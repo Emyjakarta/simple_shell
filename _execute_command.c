54cbc741b26fcc8b0226ca13956c2f440cb39287
Emyjakarta <ukonud4@gmail.com>
2023-11-11 18:00:20 +0100

    Debugging the build_path function

--- _execute_command.c
+++ _execute_command.c
@@ -7,46 +7,27 @@
  */
 void _execute_child_process(const char *_command, char **str)
 {
-	char *_path = strdup(getenv("PATH"));
-	char *_token = strtok(_path, ":");
-	char *_full_path = strdup(_token);
-	char *_temp_full_path;
-	size_t len;
+	char *_temp_full_path; 
+	char *command_array[] = {(char *)_command, NULL};
 
 	if (_command == NULL || str == NULL)
 	{
 		fprintf(stderr, "Error: command or arguments are NULL)");
-		free(_path), _path = NULL;
 		return;
 	}
-	while (_token != NULL)
+	_temp_full_path = build_path((const char **)command_array);
+	printf("Debug: Contents of the _temp_full_path: %s\n", _temp_full_path);
+	if (_temp_full_path == NULL)
 	{
-		len = strlen(_token) + strlen(_command) + 2;
-		_temp_full_path = malloc(len);
-		if (_temp_full_path == NULL)
-		{
-			perror("Memory allocation failed");
-			free(_path), _path = NULL;
-			return;
-		}
-		snprintf(_temp_full_path, len, "%s/%s", _token, _command);
-		if (access(_temp_full_path, X_OK) == 0)
-		{
-			if (execve(_temp_full_path, str, environ) == -1)
-			{
-				perror("execve");
-				free(_temp_full_path);
-				_temp_full_path = NULL;
-				exit(1);
-			}
-		}
-		free(_temp_full_path);
-		_temp_full_path = NULL;
-		_token = strtok(NULL, ":");
+		fprintf(stderr, "Command not found in PATH\n");
+		return;
 	}
-	fprintf(stderr, "Command not found in PATH\n");
-	free(_path), _path = NULL;
-	free(_full_path), _full_path = NULL;
+	if (execve(_temp_full_path, str, environ) == -1)
+	{
+		perror("execve");
+		fprintf(stderr, "Failed to execute _command: %s\n", _temp_full_path);
+	}
+	free(_temp_full_path);
 }
 /**
  * _wait_for_child_process-wait for child process
