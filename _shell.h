a73d78354692b3aa99c4fb42d73abd9bfa43b550
Emyjakarta <ukonud4@gmail.com>
2023-11-11 16:15:01 +0100

    Modified header file to include _build_path function

--- _shell.h
+++ _shell.h
@@ -89,6 +89,6 @@ void _execute_absolute_path(const char *_copy_command, char *const str[]);
 void _exe_command_from_file(const char *_filename);
 char *_replace_var(char *_command);
 int _get_exit_status(void);
-
+char *build_path(const char *_command[]);
 
 #endif /*_SHELL_H */
