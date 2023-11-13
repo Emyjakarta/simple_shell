ca737a7a611c87e1aaf6716acc9160a64453d0d1
Emyjakarta <ukonud4@gmail.com>
2023-11-13 15:42:49 +0100

    Resolved all betty errors

--- _execute_absolute_and_relative_path.c
+++ _execute_absolute_and_relative_path.c
@@ -16,19 +16,15 @@ void _execute_absolute_path(const char *_copy_command, char *const str[])
 	_copy_command_copy = strdup(_copy_command);
 	while (_token1 != NULL && MAXIMUM_ARGUMENTS + 1 > R)
 	{
-		command_array[R++] = _token1;
-		_token1 = strtok(NULL, " ");
+		command_array[R++] = _token1, _token1 = strtok(NULL, " ");
 	}
 	command_array[R] = NULL;
 	_child_pid = fork();
 	if (_child_pid == -1)
 	{
-		perror("fork");
-		free(_copy_command_copy);
+		perror("fork"), free(_copy_command_copy);
 		for (S = 0; S <= R; S++)
-		{
 			free(command_array[S]);
-		}
 		_exit(EXIT_FAILURE);
 	}
 	else if (_child_pid == 0)
@@ -37,25 +33,18 @@ void _execute_absolute_path(const char *_copy_command, char *const str[])
 		{
 			printf("Debug: Contents of the str array before execve:\n");
 			for (Q = 0; command_array[Q] != NULL; ++Q)
-			{
 				printf("[%d]: %s\n", Q, str[Q]);
-			}
 			if (execve(command_array[0], command_array, environ) == -1)
 			{
-				perror("execve failed");
-				printf("Error number: %d\n", errno);
+				perror("execve failed"), printf("Error number: %d\n", errno);
 				free(_copy_command_copy);
 				for (S = 0; S <= R; S++)
-				{
 					free(command_array[S]);
-				}
 				exit(1);
 			}
 		}
 	}
 	else
-	{
 		_wait_for_child_process(_child_pid, &_status);
-	}
 	free(_copy_command_copy);
 }
